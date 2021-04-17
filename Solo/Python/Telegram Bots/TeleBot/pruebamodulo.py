62262dc89f5754458623f4546b1ad719060fc9ae
GianK128 <66274694+GianK128@users.noreply.github.com>
2021-04-17 12:17:29 -0300

    Se quitó el token del bot
    Tremendo error feo

--- Solo/Python/Telegram Bots/TeleBot/pruebamodulo.py
+++ Solo/Python/Telegram Bots/TeleBot/pruebamodulo.py
@@ -1,5 +1,7 @@
 import modulotelegram as bot
 
+token = "Your token here."
+
 if __name__ == "__main__":
-    myBot = bot.TeleBot("1348664736:AAHO9qQEuKZIHgNf4jWFglyl7EPFCajnYAc")
+    myBot = bot.TeleBot(token)
     myBot.StartBot()
